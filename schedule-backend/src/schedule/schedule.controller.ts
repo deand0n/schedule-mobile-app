5976a3cee33d951867acd5c753b38f08362c412a
Vlad <57573532+deand0n@users.noreply.github.com>
2021-09-24 12:03:41 +0300

    backend: add: autocomplete endpoints, finished joinSameDays method, changed return types of promise methods in service

--- schedule-backend/src/schedule/schedule.controller.ts
+++ schedule-backend/src/schedule/schedule.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body } from "@nestjs/common";
+import { Controller, Get, Post, Body, Query } from "@nestjs/common";
 import { ScheduleService } from "./schedule.service";
 import { SearchParams } from "./models/search-params.model";
 
@@ -7,41 +7,50 @@ export class ScheduleController {
   constructor(private readonly scheduleService: ScheduleService) {
   }
 
-  @Get()
-  async getScheduleTest() {
-    const a = [
-      {
-        id: 0,
-        faculty: 1004,
-        group: "ПЗ-1904+ск",
-        // group: "%CF%C7-2104",
-        // group: "%CF%C7-2004",
-        // group: "%CF%C7-1904%2B%F1%EA",
-        teacher: "",
-        startDate: "17.09.2021",
-        endDate: "30.09.2021",
-        isForMonth: false,
-      },
-      {
-        id: 0,
-        faculty: 1004,
-        group: "Б-3к-121",
-        // group: "%CF%C7-2104",
-        // group: "%CF%C7-2004",
-        // group: "%CF%C7-1904%2B%F1%EA",
-        teacher: "",
-        startDate: "17.09.2021",
-        endDate: "30.09.2021",
-        isForMonth: false,
-      },
-    ];
-
-    return this.scheduleService.getSchedule(a);
-  }
+  // @Get()
+  // async getScheduleTest() {
+  //   const a = [
+  //     {
+  //       id: 0,
+  //       faculty: 1004,
+  //       group: "ПЗ-1904+ск",
+  //       // group: "%CF%C7-2104",
+  //       // group: "%CF%C7-2004",
+  //       // group: "%CF%C7-1904%2B%F1%EA",
+  //       teacher: "",
+  //       startDate: "17.09.2021",
+  //       endDate: "30.09.2021",
+  //       isForMonth: false,
+  //     },
+  //     {
+  //       id: 0,
+  //       faculty: 1004,
+  //       group: "Б-3к-121",
+  //       // group: "%CF%C7-2104",
+  //       // group: "%CF%C7-2004",
+  //       // group: "%CF%C7-1904%2B%F1%EA",
+  //       teacher: "",
+  //       startDate: "17.09.2021",
+  //       endDate: "10.10.2021",
+  //       isForMonth: false,
+  //     },
+  //   ];
+  //
+  //   return this.scheduleService.getSchedule(a);
+  // }
 
   @Post()
   async getSchedule(@Body() searchParams: SearchParams[]) {
-
     return this.scheduleService.getSchedule(searchParams);
+  }
+
+  @Get("/autocomplete/teachers")
+  async getTeacherAutocomplete(@Query("query") query: string) {
+    return await this.scheduleService.getTeacherAutocomplete(query);
+  }
+
+  @Get("/autocomplete/groups")
+  async getGroupAutocomplete(@Query("query") query: string) {
+    return await this.scheduleService.getGroupAutocomplete(query);
   }
 }
