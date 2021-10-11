3e5ce4ba294e6df42bece5b6c292a4e8d52a74b6
Vlad <57573532+deand0n@users.noreply.github.com>
2021-10-11 16:49:12 +0300

    mobile: change: default tab names

--- schedule-mobile-app/src/app/shared/models/default-settings.ts
+++ schedule-mobile-app/src/app/shared/models/default-settings.ts
@@ -1,22 +1,22 @@
-import {Settings} from './settings.model';
+import { Settings } from './settings.model';
 
 export const defaultSettings: Settings = {
   tabSettings: [
     {
       id: 0,
-      name: 'first',
+      name: '#1',
       icon: 'square',
       searchParams: []
     },
     {
       id: 1,
-      name: 'second',
+      name: '#2',
       icon: 'triangle',
       searchParams: []
     },
     {
       id: 2,
-      name: 'third',
+      name: '#3',
       icon: 'ellipse',
       searchParams: []
     }
